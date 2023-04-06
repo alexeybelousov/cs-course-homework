74aa8e92d03591d0f522ddd5ccf057cfb1187b5b
Белоусов Алексей Сергеевич (4046009) <a.belousov@vtb.ru>
2023-04-06 13:50:53 +0300

    refactoring

--- homework1/index.js
+++ homework1/index.js
@@ -1,5 +1,5 @@
 const createBitAccessor = (uint8Array) => {
-  const validate = (elIndex, bitIndex, value, callback) => {
+  const assertValidation = (elIndex, bitIndex, value) => {
     if (elIndex < 0) {
       throw new Error(`Element index ${elIndex} less than zero`);
     } else if (elIndex >= uint8Array.length) {
@@ -10,25 +10,23 @@ const createBitAccessor = (uint8Array) => {
       throw new Error(`Bit index ${bitIndex} is greater than 7`);
     } else if (typeof value !== undefined && (value < 0 || value > 1)) {
       throw new Error(`Value ${value} must be 0 or 1`);
-    } else {
-      return callback();
     }
   }
 
   return {
     get(elIndex, bitIndex) {
-      return validate(elIndex, bitIndex, null, () => {
-        return (uint8Array[elIndex] & (1 << bitIndex)) != 0 ? 1 : 0;
-      });
+      assertValidation(elIndex, bitIndex);
+      
+      return (uint8Array[elIndex] & (1 << bitIndex)) != 0 ? 1 : 0;
     },
     set(elIndex, bitIndex, value) {
-      return validate(elIndex, bitIndex, value, () => {
-        if (value) {
-          uint8Array[elIndex] = uint8Array[elIndex] | (1 << bitIndex);
-        } else {
-          uint8Array[elIndex] = uint8Array[elIndex] & ~(1 << bitIndex);
-        }
-      });
+      assertValidation(elIndex, bitIndex, value);
+
+      if (value) {
+        uint8Array[elIndex] = uint8Array[elIndex] | (1 << bitIndex);
+      } else {
+        uint8Array[elIndex] = uint8Array[elIndex] & ~(1 << bitIndex);
+      }
     }
   }
 }
