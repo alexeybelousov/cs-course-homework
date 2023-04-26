672fcafea1a6f3522d3add6c86cb9d618d90f66c
Белоусов Алексей Сергеевич (4046009) <a.belousov@vtb.ru>
2023-04-27 01:33:56 +0300

    add vector and matrix

--- homework6/matrix/index.js
+++ homework6/matrix/index.js
@@ -5,14 +5,14 @@ class Matrix3D {
   }
 
   get({ x, y, z }) {
-    return this.buffer[this.#getIndex(x, y, z)];
+    return this.buffer[this.getIndex(x, y, z)];
   }
 
   set({ x, y, z }, value) {
-    this.buffer[this.#getIndex(x, y, z)] = value;
+    this.buffer[this.getIndex(x, y, z)] = value;
   }
 
-  #getIndex(x, y, z) {
+  getIndex(x, y, z) {
     return x * this.cols + z * this.cols + y;
   }
 }
@@ -23,6 +23,6 @@ matrix.set({x: 1, y: 3, z: 2}, 10);
 matrix.set({x: 0, y: 6, z: 2}, 16);
 matrix.set({x: 9, y: 8, z: 5}, 22);
 
-console.log(matrix.get({x: 1, y: 3, z: 2}));
-console.log(matrix.get({x: 0, y: 6, z: 2}));
-console.log(matrix.get({x: 9, y: 8, z: 5}));
+console.log(matrix.get({x: 1, y: 3, z: 2})); // 10
+console.log(matrix.get({x: 0, y: 6, z: 2})); // 16
+console.log(matrix.get({x: 9, y: 8, z: 5})); // 22
--- homework6/vector/index.js
+++ homework6/vector/index.js
@@ -1,28 +1,95 @@
 class Vector {
-  constructor(TypedArray) {
-    this.buffer = new TypedArray(10);
-    this.length = this.buffer.length;
-    this.size = 0;
+  constructor(TypedArray, { capacity = 10 }) {
+    this.capacity = capacity;
+    this.arrayType = TypedArray;
+    this.buffer = new TypedArray(capacity);
+    this.length = 0;
   }
 
-  push(value) {
-    console.log(arguments.length);
+  arrayExtension(capacity) {
+    while (this.capacity < capacity) {
+      this.capacity *= 2;
+    }
 
-    this.buffer[this.size] = value;
-    this.size++;
+    const newBuffer = new this.arrayType(this.capacity);
+
+    for (let i = 0; i < this.buffer.length; i++) {
+      newBuffer[i] = this.buffer[i];
+    }
+
+    this.buffer = newBuffer;
+  }
+
+  push() {
+    if (arguments.length + this.length > this.capacity) {
+      this.arrayExtension(arguments.length + this.length);
+    }
+
+    for (let i = 0; i < arguments.length; i++) {
+      this.buffer[this.length] = arguments[i];
+      this.length++;
+    }
     
-    return this.size;
+    return this.length;
+  }
+
+  unshift() {
+    if (arguments.length + this.length > this.capacity) {
+      this.arrayExtension(arguments.length + this.length);
+    }
+
+    this.length += arguments.length;
+
+    for (let i = this.length - 1; i >= 0; i--) {
+      if (i - arguments.length >= 0) {
+        this.buffer[i] = this.buffer[i - arguments.length];
+      } else {
+        this.buffer[i] = arguments[i];
+      }
+    }
+
+    return this.length;
+  }
+
+  pop() {
+    if (this.length > 0) {
+      const value = this.buffer[this.length - 1];
+
+      this.buffer[this.length - 1] = undefined;
+      this.length -= 1;
+
+      return value;
+    } else {
+      return undefined;
+    }
+  }
+
+  shift() {
+    if (this.length > 0) {
+      const value = this.buffer[0];
+
+      for (let i = 0; i < this.length - 1; i++) {
+        this.buffer[i] = this.buffer[i + 1];
+      }
+      
+      this.buffer[this.length - 1] = undefined;
+      this.length -= 1;
+
+      return value;
+    } else {
+      return undefined;
+    }
   }
 }
 
 
-const uint8Vector = new Vector(Uint8Array, {capacity: 100});
+const uint8Vector = new Vector(Uint8Array, { capacity: 5 });
 
-console.log(uint8Vector.push(100));    // 1
-console.log(uint8Vector.push(20, 10)); // 3
+uint8Vector.push(100);    // 1
+uint8Vector.push(20, 10); // 3
 
-// uint8Vector.pop();        // 10
-// uint8Vector.shift();      // 100
+uint8Vector.pop();        // 10
+uint8Vector.shift();      // 100
 
-// uint8Vector.unshift(1);          // 2
+uint8Vector.unshift(1);          // 2
 console.log(uint8Vector.length); // 2
